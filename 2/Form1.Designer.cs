e3e58414a5ce181bc0a0b9b87d53b4b11e4a208f
danil <danil@DESKTOP-P7OT76A>
2025-11-04 21:20:26 +0300

    Добавьте файлы проекта.

---
+++ 2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_in = new System.Windows.Forms.TextBox();
+            this.patternBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.showTableButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текст для поиска:";
+            // 
+            // txt_in
+            // 
+            this.txt_in.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txt_in.Location = new System.Drawing.Point(20, 50);
+            this.txt_in.Name = "txt_in";
+            this.txt_in.Size = new System.Drawing.Size(400, 24);
+            this.txt_in.TabIndex = 1;
+            // 
+            // patternBox
+            // 
+            this.patternBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.patternBox.Location = new System.Drawing.Point(23, 120);
+            this.patternBox.Name = "patternBox";
+            this.patternBox.Size = new System.Drawing.Size(200, 24);
+            this.patternBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Искомая подстрока:";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultBox.Location = new System.Drawing.Point(20, 191);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultBox.Size = new System.Drawing.Size(650, 250);
+            this.resultBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Результаты:";
+            // 
+            // searchButton
+            // 
+            this.searchButton.BackColor = System.Drawing.Color.Teal;
+            this.searchButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.searchButton.ForeColor = System.Drawing.Color.White;
+            this.searchButton.Location = new System.Drawing.Point(240, 116);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(120, 33);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "Найти подстроку";
+            this.searchButton.UseVisualStyleBackColor = false;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // showTableButton
+            // 
+            this.showTableButton.BackColor = System.Drawing.Color.Teal;
+            this.showTableButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.showTableButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.showTableButton.ForeColor = System.Drawing.Color.White;
+            this.showTableButton.Location = new System.Drawing.Point(370, 116);
+            this.showTableButton.Name = "showTableButton";
+            this.showTableButton.Size = new System.Drawing.Size(187, 33);
+            this.showTableButton.TabIndex = 7;
+            this.showTableButton.Text = "Показать таблицу";
+            this.showTableButton.UseVisualStyleBackColor = false;
+            this.showTableButton.Click += new System.EventHandler(this.showTableButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(686, 453);
+            this.Controls.Add(this.showTableButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.patternBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_in);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поиск подстроки - Конечный автомат";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_in;
+        private System.Windows.Forms.TextBox patternBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button showTableButton;
+    }
+}
+
---
+++ 2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _2
+{
+    /// Главная форма приложения для поиска подстрок с использованием конечного автомата
+    public partial class Form1 : Form
+    {
+        private FiniteStateAutomaton automaton; // Экземпляр конечного автомата
+
+        public Form1()
+        {
+            InitializeComponent();
+            automaton = new FiniteStateAutomaton(); // Инициализация автомата
+        }
+
+        // Обработчик события поиска подстроки в тексте
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string text = txt_in.Text;       // Исходный текст для поиска
+            string pattern = patternBox.Text; // Подстрока (шаблон) для поиска
+
+            // Проверка ввода подстроки
+            if (string.IsNullOrEmpty(pattern))
+            {
+                resultBox.Text = "Введите подстроку для поиска!";
+                return;
+            }
+
+            // Построение автомата для заданного шаблона и поиск вхождений
+            automaton.BuildAutomaton(pattern);
+            List<int> positions = automaton.Search(text);
+
+            // Формирование результата поиска
+            resultBox.Text = $"Поиск подстроки: '{pattern}' в тексте: '{text}'\r\n";
+            resultBox.Text += $"Длина подстроки: {pattern.Length}\r\n";
+            resultBox.Text += $"Количество состояний автомата: {pattern.Length + 1}\r\n\r\n";
+
+            // Вывод результатов поиска
+            if (positions.Count > 0)
+            {
+                resultBox.Text += $"Найдено в позициях: {string.Join(", ", positions)}\r\n";
+                resultBox.Text += $"Всего вхождений: {positions.Count}";
+            }
+            else
+            {
+                resultBox.Text += "Подстрока не найдена";
+            }
+        }
+
+        // Обработчик события для отображения таблицы переходов автомата
+        private void showTableButton_Click(object sender, EventArgs e)
+        {
+            string pattern = patternBox.Text; // Подстрока для построения таблицы
+
+            // Проверка ввода подстроки
+            if (string.IsNullOrEmpty(pattern))
+            {
+                resultBox.Text = "Введите подстроку для построения таблицы!";
+                return;
+            }
+
+            // Построение автомата и вывод таблицы переходов
+            automaton.BuildAutomaton(pattern);
+            resultBox.Text = automaton.AnalyzeAutomaton();
+        }
+    }
+
+    /// Класс, реализующий конечный автомат для поиска подстрок
+    /// Использует алгоритм построения таблицы переходов для эффективного поиска
+    public class FiniteStateAutomaton
+    {
+        private int[,] transitionTable; // Таблица переходов между состояниями
+        private string pattern;         // Шаблон для поиска
+        private int patternLength;      // Длина шаблона
+
+        /// Построение автомата для заданного шаблона
+        /// Создает таблицу переходов размером (patternLength+1) x 256
+
+        public void BuildAutomaton(string pattern)
+        {
+            this.pattern = pattern;
+            this.patternLength = pattern.Length;
+
+            int totalStates = patternLength + 1; // Количество состояний (включая начальное и конечное)
+            transitionTable = new int[totalStates, 256]; // Таблица для ASCII символов
+
+            // Заполнение таблицы переходов
+            for (int state = 0; state < totalStates; state++)
+            {
+                for (int ch = 0; ch < 256; ch++)
+                {
+                    if (state < patternLength && (char)ch == pattern[state])
+                    {
+                        // Переход к следующему состоянию при совпадении символа
+                        transitionTable[state, ch] = state + 1;
+                    }
+                    else
+                    {
+                        // Поиск наиболее длинного префикса, который является суффиксом
+                        string current = pattern.Substring(0, state) + (char)ch;
+                        transitionTable[state, ch] = FindLongestPrefixSuffix(current, pattern);
+                    }
+                }
+            }
+        }
+
+        /// Поиск длины наибольшего префикса шаблона, который является суффиксом текущей строки
+        /// </summary>
+        /// <param name="text">Текущая строка</param>
+        /// <param name="pattern">Шаблон поиска</param>
+        /// <returns>Длина наибольшего совпадения</returns>
+        private int FindLongestPrefixSuffix(string text, string pattern)
+        {
+            int maxLength = Math.Min(text.Length, pattern.Length);
+
+            // Поиск от максимальной возможной длины к минимальной
+            for (int length = maxLength; length > 0; length--)
+            {
+                if (text.EndsWith(pattern.Substring(0, length)))
+                {
+                    return length;
+                }
+            }
+
+            return 0; // Нет совпадений - возврат в начальное состояние
+        }
+
+        /// Поиск всех вхождений шаблона в тексте с использованием построенного автомата
+        /// <param name="text">Текст для поиска</param>
+        /// <returns>Список позиций начала вхождений</returns>
+        public List<int> Search(string text)
+        {
+            List<int> positions = new List<int>(); // Список найденных позиций
+            int currentState = 0;                  // Текущее состояние автомата
+
+            // Посимвольный проход по тексту
+            for (int i = 0; i < text.Length; i++)
+            {
+                // Переход в следующее состояние по текущему символу
+                currentState = transitionTable[currentState, text[i]];
+
+                // Если достигнуто конечное состояние - найдено вхождение
+                if (currentState == patternLength)
+                {
+                    positions.Add(i - patternLength + 1); // Добавление позиции начала вхождения
+                }
+            }
+
+            return positions;
+        }
+
+        /// Анализ автомата и построение текстового представления таблицы переходов
+        /// <returns>Строка с подробным анализом автомата</returns>
+        public string AnalyzeAutomaton()
+        {
+            string analysis = "ТАБЛИЦА ПЕРЕХОДОВ ДЛЯ ПОДСТРОКИ: " + pattern + "\r\n\r\n";
+            analysis += $"Количество состояний: {patternLength + 1}\r\n";
+            analysis += "Конечное состояние: q" + patternLength + "\r\n\r\n";
+
+            // Создаем таблицу с правильными отступами
+            analysis += "Состояние | ";
+
+            // Заголовки для символов из шаблона
+            foreach (char ch in pattern)
+            {
+                analysis += $"{ch} | ";
+            }
+            analysis += "другие\r\n";
+
+            // Разделительная линия
+            analysis += new string('-', 12 + pattern.Length * 5) + "\r\n";
+
+            // Данные таблицы переходов
+            for (int state = 0; state <= patternLength; state++)
+            {
+                analysis += $"q{state}".PadRight(9) + " | ";
+
+                // Переходы для символов из шаблона
+                foreach (char ch in pattern)
+                {
+                    int nextState = transitionTable[state, ch];
+                    analysis += $"q{nextState}".PadRight(2) + " | ";
+                }
+
+                // Переход для других символов (в качестве примера используется 'x')
+                int otherState = transitionTable[state, 'x'];
+                analysis += $"q{otherState}";
+
+                analysis += "\r\n";
+            }
+
+            // Добавление пояснений
+            analysis += "\r\n" + new string('=', 60) + "\r\n\r\n";
+            analysis += "ОБЪЯСНЕНИЕ ПЕРЕХОДОВ:\r\n";
+            analysis += "• q₀ - начальное состояние\r\n";
+            analysis += "• При правильном символе - переход к следующему состоянию\r\n";
+            analysis += "• При неправильном символе - переход к состоянию, соответствующему\r\n";
+            analysis += "  наибольшему префиксу, который является суффиксом текущей строки\r\n\r\n";
+
+            return analysis;
+        }
+    }
+}
